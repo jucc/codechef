0d4946971a6885cdc2d2f5aa62069ebc2c342b78
Juliana Correa <jucorrea.mail@gmail.com>
2012-09-29 17:29:14 -0300

    adding python solution and sample inputs

---
+++ test.py
@@ -0,0 +1,13 @@
+"""
+    codechef.com - Practice problem 01 - Easy
+    Life, the Universe and Everything - http://www.codechef.com/problems/TEST
+    Language - Python
+    Juliana Correa
+"""
+ 
+guess = int(raw_input())
+ 
+while guess != 42:        
+    print guess
+    guess = int(raw_input())
+        
