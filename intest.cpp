ea77c1baad300c19e5c19e4366adf4b32a944c47
Juliana <jucorrea.mail@gmail.com>
2014-06-18 19:41:01 -0300

    Enormous input problem with cin solved. Very interesting, indeed, follow the links

--- intest.cpp
+++ intest.cpp
@@ -7,8 +7,15 @@
 
 #include <iostream>
     
-using namespace std;        
+using namespace std;       
+ 
 int main() {
+
+   // Oh, dear, cin is so much more complicated than I thought!! :S 
+   // Without this sync, it gets as fast as C-style input (scanf, fread). More info at:
+   // http://www.drdobbs.com/the-standard-librarian-iostreams-and-std/184401305
+   // http://codeforces.com/blog/entry/925
+   std::ios_base::sync_with_stdio(false);
 
    int n, k, x, divs = 0;
    cin >> n >> k;
