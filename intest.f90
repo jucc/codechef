fd54f1ab78b027bca6691f20e7c6208979c6b400
Juliana <jucorrea.mail@gmail.com>
2014-06-19 15:49:54 -0300

    Fortran revised solution for enormous input. In order to pass the time constraint, the entire input was read at once and kept at memory for iteration, instead of iterating while reading and discarding input

--- intest.f90
+++ intest.f90
@@ -3,22 +3,44 @@
 !    Language - Fortran
 !    Juliana Correa
 
+module mdivs
+   implicit none
+   contains
+
+   function divs (n, k)
+   
+      integer, intent(in) :: n, k
+      integer, dimension(n) :: input
+      integer :: divs, i
+
+      ! in order to be time-efficient, we sacrifice memory, reading the entire input 
+      ! and keeping it, instead of reading in a loop
+      read *, input
+      i = 1
+      divs = 0
+      do while (i <= n)
+         if (mod(input(i), k) == 0) then
+            divs = divs + 1
+         end if
+         i = i + 1
+      end do
+
+   end function divs
+
+end module mdivs
+
+
 program enormous
+   use mdivs
    implicit none
 
-   integer :: n, k, x, i, divs
+   integer :: n, k
+   
    read *, n, k
-
-   i = 0
-   divs = 0
-   do while (i < n)
-      read *, x
-      if (mod(x,k) == 0) then
-         divs = divs + 1
-      end if
-      i = i + 1
-   end do
-
-   print *, divs
+   if (n /= 0 .and. k /= 0) then
+      print *, divs(n, k)
+   else
+      print *, 0
+   end if
 
 end program
